7bb4f189c79752bae6723b9f1a8a5d3873cd7291
= <albakrieee@gmail.com>
2023-10-06 03:24:36 +0000

    fix: port already in use while testing

--- app/server.ts
+++ app/server.ts
@@ -5,9 +5,9 @@ import config from '../config'
 const srv = createServer(app)
 
 export function startServer(): void {
-	srv.listen(config.port, () => {
-		console.log(`Magazine hub - backend listening on port: ${config.port}`)
-	})
+	//to prevent listening on port during testing to avoid 'Port already in use issue'
+	if(config.node_env === 'test') return;
+	srv.listen(config.port, () => console.log(`Magazine hub - backend listening on port: ${config.port}`))
 }
 
 export function closeServer(): void {
--- config/index.ts
+++ config/index.ts
@@ -3,4 +3,5 @@ require('dotenv').config()
 export default {
     port: process.env.MAGAZINE_HUB_PORT,
     jwt_key: process.env.MAGAZINE_HUB_JWT_KEY,
+    node_env: process.env.NODE_ENV,
 }
