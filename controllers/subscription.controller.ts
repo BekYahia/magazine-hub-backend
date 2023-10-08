a48a5c95b19cbb2158569401d11dd854f0804235
= <albakrieee@gmail.com>
2023-10-08 23:48:02 +0000

    fix(test): resolve issues with failed subscription test suites

--- controllers/subscription.controller.ts
+++ controllers/subscription.controller.ts
@@ -6,26 +6,28 @@ import Magazine from '../models/magazine'
 export default {
 
     async all(req: Request, res: Response) {
-        
+
         //parse boolean
         const where = req.query
-        if (where?.is_active !== undefined) where.is_active = JSON.parse(where.is_active as string)    
-    
-        const magazines = await Subscription.findAll({ order: [['id', 'DESC']], include: [
-            { model: User, attributes: ['id', 'name', 'email'] },
-            { model: Magazine, attributes: ['id', 'title', 'is_active'] },
-        ],
-        where,
-    })
-        res.send(magazines)
+        if (where?.is_active !== undefined) where.is_active = JSON.parse(where.is_active as string)
+
+        const subscription = await Subscription.findAll({
+            order: [['id', 'DESC']],
+            include: [
+                { model: User, attributes: ['id', 'name', 'email'] },
+                { model: Magazine, attributes: ['id', 'title', 'is_active'] },
+            ],
+            where,
+        })
+        res.send(subscription)
     },
 
     async byId(req: Request, res: Response) {
-		const magazine = await Subscription.findByPk(req.params.id)
-        if(!magazine) return res.status(404).json('No subscription found')
-		res.send(magazine)
+        const subscription = await Subscription.findByPk(req.params.id)
+        if (!subscription) return res.status(404).json('No subscription found')
+        res.send(subscription)
     },
-	
+
     async create(req: Request, res: Response) {
 
         // check if user exist
@@ -34,56 +36,60 @@ export default {
 
         // check if magazine exist
         const isMagazine = await Magazine.findByPk(req.body.MagazineId)
-        if (!isMagazine) return res.status(400).json('No Magazine found with this id')
+        if (!isMagazine) return res.status(400).json('No magazine found with this id')
 
-		// is active subscription
-		const isSubscription = await Subscription.findOne({ where: {
-            UserId: req.body.UserId,
-            MagazineId: req.body.MagazineId,
-            is_active: true,
-        }})
-		if (isSubscription) return res.status(400).json('You already have an active subscription for this magazine')
+        // is active subscription
+        const isSubscription = await Subscription.findOne({
+            where: {
+                UserId: req.body.UserId,
+                MagazineId: req.body.MagazineId,
+                is_active: true,
+            }
+        })
+        if (isSubscription) return res.status(400).json('You already have an active subscription for this magazine')
 
-		//save
-		const magazine = await Subscription.create(req.body)
+        //save
+        const subscription = await Subscription.create(req.body)
 
         //payment with stripe
-            //- success? update {payment_status: 'paid', is_active: true}
-            //- notify user via email
+        //- success? update {payment_status: 'paid', is_active: true}
+        //- notify user via email
 
-		res.status(201).send(magazine)
+        res.status(201).send(subscription)
     },
 
-   async update(req: any, res: Response) {
-	   //is valid subscription
-	   let isSubscription = await Subscription.findByPk(req.params.id)
-	   if(!isSubscription) return res.status(404).json('No subscription found')
+    async update(req: any, res: Response) {
+        //is valid subscription
+        let isSubscription = await Subscription.findByPk(req.params.id)
+        if (!isSubscription) return res.status(404).json('No subscription found')
 
-		//update
-		let magazine = await Subscription.update(req.body, {
-			where: { id: req.params.id },
-			individualHooks: true
-		})
-		//@ts-ignore
-		res.json(magazine[1][0])
+        //update
+        let subscription = await Subscription.update(req.body, {
+            where: { id: req.params.id },
+            individualHooks: true
+        })
+        //@ts-ignore
+        res.json(subscription[1][0])
     },
 
     async cancelSubscription(req: any, res: Response) {
-	   //is active subscription
-       const isSubscription = await Subscription.findOne({ where: {
-            UserId: req.body.UserId,
-            MagazineId: req.body.MagazineId,
-            is_active: true,
-        }})
-       if(!isSubscription) return res.status(400).json('you don\'t have an active subscription')
+        //is active subscription
+        const isSubscription = await Subscription.findOne({
+            where: {
+                UserId: req.body.UserId,
+                MagazineId: req.body.MagazineId,
+                is_active: true,
+            }
+        })
+        if (!isSubscription) return res.status(400).json('you don\'t have an active subscription')
 
-		//update
-		let magazine = await Subscription.update({ is_active: false }, {
-			where: { id: isSubscription.id },
-			individualHooks: true
-		})
-		//@ts-ignore
-		res.json(magazine[1][0])        
+        //update
+        let subscription = await Subscription.update({ is_active: false }, {
+            where: { id: isSubscription.id },
+            individualHooks: true
+        })
+        //@ts-ignore
+        res.json(subscription[1][0])
     },
 
 }
--- tests/subscriptions.test.ts
+++ tests/subscriptions.test.ts
@@ -1,123 +1,359 @@
-import User from '../models/user'
-import Magazine from '../models/magazine'
-import Subscription from '../models/subscription'
-import request from 'supertest'
-import { app } from '..'
-import { closeServer } from '../app/server'
+import User from "../models/user";
+import Magazine from "../models/magazine";
+import Subscription from "../models/subscription";
+import request from "supertest";
+import { app } from "..";
+import { closeServer } from "../app/server";
 
-describe('Subscriptions API', () => {
-    let token = ''
-    let userId = 0
-    let magazineId = 0
+describe("Subscriptions API", () => {
+	let token = "";
+	let userId = 0;
+	let magazineId = 0;
 
-    afterEach(async () => {
-        await User.destroy({ where: {} })
-        await Magazine.destroy({ where: {} })
-        await Subscription.destroy({ where: {} })
-        closeServer()
-    })
+	const user = {
+		name: "John Doe",
+		email: "j@doe.com",
+		password: "1234",
+	};
+	const magazine = {
+		title: "Magazine",
+		description: "Magazine description",
+		price: 10.0,
+		publication_date: new Date(),
+		is_active: true,
+	};
+	const subscription = {
+		MagazineId: 1,
+		UserId: 1,
+		is_active: true,
+	};
 
-    const user = {
-        name: 'John Doe',
-        email: "j@doe.com",
-        password: "1234"
-    }
-    const magazine = {
-        title: 'Magazine',
-        description: 'Magazine description',
-        price: 10.00,
-        publication_date: new Date(),
-        is_active: true,
-    }
-    const subscription = {
-        MagazineId: 1,
-        UserId: 1,
-        is_active: true,
-    }
+	afterEach(async () => {
+		await Subscription.destroy({ where: {} });
+		await User.destroy({ where: {} });
+		await Magazine.destroy({ where: {} });
+		closeServer();
+	});
 
-    describe('POST Prep user nad magazine', () => {
+	beforeAll(async () => {
+		const usr = await request(app).post("/api/users").send(user);
+		const res = await request(app)
+			.post("/api/users/login")
+			.send({ email: user.email, password: user.password });
+		token = res.header["x-auth-token"];
+	});
 
-        it('login successfully, to use it the other requests', async () => {
-            const usr = await request(app).post('/api/users').send(user)
-            const res = await request(app).post('/api/users/login').send({ email: user.email, password: user.password })
+	describe("POST /api/subscriptions", () => {
+		it("Create new subscription", async () => {
+			const random = Math.floor(Math.random() * 10000);
 
-            token = res.header['x-auth-token']
-            userId = usr.body.id
-            expect(res.status).toBe(200)
-            expect(res.header['x-auth-token']).toBeTruthy()
-        })
+			//create user
+			const usr = await request(app)
+				.post("/api/users")
+				.send({
+					...user,
+					email: `j@doe${random}.com`,
+				});
 
-		it('create new magazine', async () => {
-			const res = await request(app).post('/api/magazines').send(magazine)
-            magazineId = res.body.id
-			expect(res.status).toBe(200)
-			expect(res.body.title).toBe(magazine.title)
-		})
-    })
-
-	describe('POST /api/subscriptions', () => {
-
-		it('create new subscription', async () => {
-			const res = await request(app).post('/api/subscriptions').send(subscription)
-
-			expect(res.status).toBe(200)
-			expect(res.body.MagazineId).toBe(subscription.MagazineId)
-		})
-
-
-		it('shouldn\'t create new subscription, missing fields', async () => {
-			const res = await request(app).post('/api/subscriptions').send({})
-			expect(res.status).toBe(400)
-			expect(res.body._original).toBeTruthy()
-		})
-
-		it('shouldn\'t create new subscription, you already have an active subscriptions', async () => {
-
-            //check if have a subscription with same user_id and magazine_id and is_active = true
-			const res = await request(app).post('/api/subscriptions').send(subscription) //same subscription
-			expect(res.status).toBe(400)
-			expect(res.body._original).toBeTruthy()
-		})
-	})
-
-	describe('GET /api/subscriptions', () => {
-		it('get all subscriptions', async () => {
-			const res = await request(app).get('/api/subscriptions').set('x-auth-token', token)
-			expect(res.status).toBe(200)
-			expect(Array.isArray(res.body)).toBe(true)
-		})
-	})
-
-
-	describe('GET /api/subscriptions/:id', () => {
-
-		it('get subscription', async () => {
+			//create magazine
+			const mag = await request(app)
+				.post("/api/magazines")
+				.send({
+					...magazine,
+					title: `Magazine ${random}`,
+				})
+				.set("x-auth-token", token);
 
 			//create subscription
-			const sub = await request(app).post('/api/subscriptions').send(subscription)
+			const res = await request(app)
+				.post("/api/subscriptions")
+				.send({
+					UserId: usr.body.id,
+					MagazineId: mag.body.id,
+					start_date: new Date(),
+					end_date: new Date(new Date().setMonth(new Date().getMonth() + 1)),
+				})
+				.set("x-auth-token", token);
 
-			//get user data
-			const res = await request(app).get(`/api/subscriptions/${sub.body.id}`).set('x-auth-token', token)
+			expect(res.status).toBe(201);
+			expect(res.body.UserId).toBe(usr.body.id);
+			expect(res.body.MagazineId).toBe(mag.body.id);
+		});
 
-			expect(res.status).toBe(200)
-			expect(res.body.MagazineId).toBe(subscription.MagazineId)
-		})
+		it("Shouldn't create new subscription - Invalid user id", async () => {
+			const random = Math.floor(Math.random() * 10000);
 
-		it('should\'t get subscription, invalid id', async () => {
-			const res = await request(app).get('/api/subscriptions/s4').set('x-auth-token', token)
-			expect(res.status).toBe(400)
-			expect(res.body._original.id).toBe('s4')
-		})
-	})
+			//create magazine
+			const mag = await request(app)
+				.post("/api/magazines")
+				.send({
+					...magazine,
+					title: `Magazine ${random}`,
+				})
+				.set("x-auth-token", token);
 
-    describe('POST /api/subscriptions/cancel', () => {
-		it('cancel subscription', async () => {
-			const sub = await request(app).post('/api/subscriptions').send(subscription)
-			const res = await request(app).post(`/api/subscriptions/cancel`).send({ is_active: false, UserId: userId, MagazineId: magazineId }).set('x-auth-token', token)
+			//create subscription
+			const res = await request(app)
+				.post("/api/subscriptions")
+				.send({
+					UserId: random,
+					MagazineId: mag.body.id,
+					start_date: new Date(),
+					end_date: new Date(new Date().setMonth(new Date().getMonth() + 1)),
+				})
+				.set("x-auth-token", token);
 
-			expect(res.status).toBe(200)
-			expect(res.body.is_active).toBe(false)
-		})
+			expect(res.status).toBe(400);
+			expect(res.body).toBe("No user found with this id");
+		});
+
+		it("Shouldn't create new subscription - Invalid Magazine id", async () => {
+			const random = Math.floor(Math.random() * 10000);
+
+			//create user
+			const usr = await request(app)
+				.post("/api/users")
+				.send({
+					...user,
+					email: `j@doe${random}.com`,
+				});
+
+			//create subscription
+			const res = await request(app)
+				.post("/api/subscriptions")
+				.send({
+					UserId: usr.body.id,
+					MagazineId: random,
+					start_date: new Date(),
+					end_date: new Date(new Date().setMonth(new Date().getMonth() + 1)),
+				})
+				.set("x-auth-token", token);
+
+			expect(res.status).toBe(400);
+			expect(res.body).toBe("No magazine found with this id");
+		});
+
+		it("Shouldn't create new subscription - Already have an active subscription to this magazine", async () => {
+			const random = Math.floor(Math.random() * 10000);
+
+			//create user
+			const usr = await request(app)
+				.post("/api/users")
+				.send({
+					...user,
+					email: `j@doe${random}.com`,
+				});
+
+			//create magazine
+			const mag = await request(app)
+				.post("/api/magazines")
+				.send({
+					...magazine,
+					title: `Magazine ${random}`,
+				})
+				.set("x-auth-token", token);
+
+			const subscription = {
+				UserId: usr.body.id,
+				MagazineId: mag.body.id,
+				start_date: new Date(),
+				end_date: new Date(new Date().setMonth(new Date().getMonth() + 1)),
+			};
+			//create subscription
+			const sub = await request(app)
+				.post("/api/subscriptions")
+				.send(subscription)
+				.set("x-auth-token", token);
+
+			//activate subscription
+			await request(app)
+				.put(`/api/subscriptions/${sub.body?.id}`)
+				.send({ is_active: true })
+				.set("x-auth-token", token);
+
+			//try create an other subscription
+			const res = await request(app)
+				.post("/api/subscriptions")
+				.send(subscription)
+				.set("x-auth-token", token);
+
+			expect(res.status).toBe(400);
+			expect(res.body).toBe(
+				"You already have an active subscription for this magazine"
+			);
+		});
+
+		it("Shouldn't create new subscription - missing fields", async () => {
+			const res = await request(app)
+				.post("/api/subscriptions")
+				.send({})
+				.set("x-auth-token", token);
+
+			expect(res.status).toBe(400);
+			expect(res.body._original).toBeTruthy();
+		});
+	});
+
+	describe("GET /api/subscriptions", () => {
+		it("Get all subscriptions - Empty array", async () => {
+			const res = await request(app)
+				.get("/api/subscriptions")
+				.set("x-auth-token", token);
+
+			expect(res.status).toBe(200);
+			expect(Array.isArray(res.body)).toBe(true);
+			expect(res.body.length).toBe(0);
+		});
+
+		it("Get all subscriptions - Not Empty array", async () => {
+			const random = Math.floor(Math.random() * 10000);
+
+			//create user
+			const usr = await request(app)
+				.post("/api/users")
+				.send({
+					...user,
+					email: `j@doe${random}.com`,
+				});
+
+			//create magazine
+			const mag = await request(app)
+				.post("/api/magazines")
+				.send({
+					...magazine,
+					title: `Magazine ${random}`,
+				})
+				.set("x-auth-token", token);
+
+			//create subscription
+			const sub = await request(app)
+				.post("/api/subscriptions")
+				.send({
+					UserId: usr.body.id,
+					MagazineId: mag.body.id,
+					start_date: new Date(),
+					end_date: new Date(new Date().setMonth(new Date().getMonth() + 1)),
+				})
+				.set("x-auth-token", token);
+
+			const res = await request(app)
+				.get("/api/subscriptions")
+				.set("x-auth-token", token);
+
+			expect(res.status).toBe(200);
+			expect(Array.isArray(res.body)).toBe(true);
+			expect(res.body.length).toBe(1);
+		});
+	});
+
+	describe("GET /api/subscriptions/:id", () => {
+		it("Get subscription", async () => {
+			const random = Math.floor(Math.random() * 10000);
+
+			//create user
+			const usr = await request(app)
+				.post("/api/users")
+				.send({
+					...user,
+					email: `j@doe${random}.com`,
+				});
+
+			//create magazine
+			const mag = await request(app)
+				.post("/api/magazines")
+				.send({
+					...magazine,
+					title: `Magazine ${random}`,
+				})
+				.set("x-auth-token", token);
+
+			//create subscription
+			const sub = await request(app)
+				.post("/api/subscriptions")
+				.send({
+					UserId: usr.body.id,
+					MagazineId: mag.body.id,
+					start_date: new Date(),
+					end_date: new Date(new Date().setMonth(new Date().getMonth() + 1)),
+				})
+				.set("x-auth-token", token);
+
+			//get subscription
+			const res = await request(app)
+				.get(`/api/subscriptions/${sub.body.id}`)
+				.set("x-auth-token", token);
+
+			expect(res.status).toBe(200);
+			expect(Number.isNaN(res.body.id)).toBe(false);
+		});
+
+		it("Should't get subscription - invalid id (not a number)", async () => {
+			const res = await request(app)
+				.get("/api/subscriptions/s4")
+				.set("x-auth-token", token);
+
+			expect(res.status).toBe(400);
+			expect(res.body._original.id).toBeTruthy();
+		});
+
+		it("Should't get subscription - not found", async () => {
+			const random = Math.floor(Math.random() * 10000);
+			const res = await request(app)
+				.get(`/api/subscriptions/${random}`)
+				.set("x-auth-token", token);
+
+			expect(res.status).toBe(404);
+			expect(res.body).toBe("No subscription found");
+		});
+	});
+
+	describe("POST /api/subscriptions/cancel", () => {
+		it("Should cancel subscription", async () => {
+			const random = Math.floor(Math.random() * 10000);
+
+			//create user
+			const usr = await request(app)
+				.post("/api/users")
+				.send({
+					...user,
+					email: `j@doe${random}.com`,
+				});
+
+			//create magazine
+			const mag = await request(app)
+				.post("/api/magazines")
+				.send({
+					...magazine,
+					title: `Magazine ${random}`,
+				})
+				.set("x-auth-token", token);
+
+			//create subscription
+			const sub = await request(app)
+				.post("/api/subscriptions")
+				.send({
+					UserId: usr.body.id,
+					MagazineId: mag.body.id,
+					start_date: new Date(),
+					end_date: new Date(new Date().setMonth(new Date().getMonth() + 1)),
+				})
+				.set("x-auth-token", token);
+
+			//activate subscription
+			const activate = await request(app)
+				.put(`/api/subscriptions/${sub.body.id}`)
+				.send({ is_active: true })
+				.set("x-auth-token", token);
+
+			//cancel subscription
+			const res = await request(app)
+				.post(`/api/subscriptions/cancel`)
+				.send({ UserId: usr.body.id, MagazineId: mag.body.id })
+				.set("x-auth-token", token);
+
+			expect(activate.body.is_active).toBe(true); //First, it's been activated
+			expect(res.body.is_active).toBe(false); //Then, canceled
+		});
 
 		// it('shouldn\'t cancel subscription, no subscription with this ID', async () => {
 		// 	const sub = await request(app).post('/api/subscriptions').send(subscription)
@@ -127,35 +363,45 @@ describe('Subscriptions API', () => {
 		// 	expect(res.body).toBe('No subscription found')
 		// })
 
-		it('shouldn\'t cancel subscription, no active subscription to be canceled', async () => {
-			const subscription2 = {
-				...subscription,
-                is_active: false,
-			}
-			const sub = await request(app).post('/api/subscriptions').send(subscription2)
-			const res = await request(app).post(`/api/subscriptions/cancel`).send({ is_active: false, UserId: subscription.UserId, MagazineId: subscription.MagazineId }).set('x-auth-token', token)
+		it("Shouldn't cancel subscription - no active subscription to be canceled", async () => {
+			const random = Math.floor(Math.random() * 10000);
 
-			expect(res.status).toBe(400)
-		})
-    })
+			//create user
+			const usr = await request(app)
+				.post("/api/users")
+				.send({
+					...user,
+					email: `j@doe${random}.com`,
+				});
 
+			//create magazine
+			const mag = await request(app)
+				.post("/api/magazines")
+				.send({
+					...magazine,
+					title: `Magazine ${random}`,
+				})
+				.set("x-auth-token", token);
 
-	describe('DELETE /api/subscriptions/:id', () => {
+			//create subscription
+			const sub = await request(app)
+				.post("/api/subscriptions")
+				.send({
+					UserId: usr.body.id,
+					MagazineId: mag.body.id,
+					start_date: new Date(),
+					end_date: new Date(new Date().setMonth(new Date().getMonth() + 1)),
+				})
+				.set("x-auth-token", token);
 
-		it('soft delete subscription', async () => {
-			const sub = await request(app).post('/api/subscriptions').send(subscription)
-			const res = await request(app).delete(`/api/subscriptions/${sub.body.id}`).set('x-auth-token', token)
+			//cancel subscription
+			const res = await request(app)
+				.post(`/api/subscriptions/cancel`)
+				.send({ UserId: usr.body.id, MagazineId: mag.body.id })
+				.set("x-auth-token", token);
 
-			expect(res.status).toBe(200)
-			expect(res.body.del).toBe(1)
-		})
-
-		it('should\'t delete subscription, invalid id', async () => {
-			const res = await request(app).delete('/api/subscriptions/a4').set('x-auth-token', token)
-			expect(res.status).toBe(400)
-			expect(res.body._original.id).toBe('a4')
-		})
-	})
-
-
+			expect(res.status).toBe(400);
+			expect(res.body).toBe("you don't have an active subscription");
+		});
+	});
 });
